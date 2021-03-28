86ddc7943cfb6e7d2dd2af4bd3f1495b66fe1ddb
pbsmith82 <pbsmith82@gmail.com>
2021-03-28 19:58:16 -0400

    Done.

--- main.js
+++ main.js
@@ -1,11 +1,51 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+const errorModal = document.querySelector("#modal")
 // Your JavaScript code goes here!
+//------------------------------------------------------------------------------
+// Start Hiding Error Modal
+function hideModal() {
+ errorModal.className = "hidden"
+}
+hideModal()
+// End Hiding Error Modal
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Start Like Behavior
+document.addEventListener("click", (e) => {
+  if (e.target.className == "like-glyph" || "activated-heart")
+    if (e.target.innerText == EMPTY_HEART)
+    mimicServerCall()
+    .then(function(response) {
+      return response;
+    }).then(function() {
+      e.target.innerText = FULL_HEART
+      e.target.className = "activated-heart"
+    }).catch(function(error) {
+      errorModal.className = ""
+      errorModal.innerText = error
+      setTimeout(function(){ hideModal() }, 5000)
+    })
+    else if (e.target.className == "activated-heart")
+    mimicServerCall()
+    .then(function(response) {
+      return response;
+    }).then(function() {
+      e.target.innerText = EMPTY_HEART
+      e.target.className = "like-glyph"
+    }).catch(function(error) {
+      errorModal.className = ""
+      errorModal.innerText = error
+      setTimeout(function(){ hideModal() }, 5000)
+    })
+    //   e.target.innerText = FULL_HEART
+    // else
+    //   e.target.innerText = EMPTY_HEART  
+})
 
-
-
+// End Like Behavior
+//------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
